f3ad62e0962a244caf00d60768234d6b8820150f
Rodrigo Teixeira <r.teixeira1@outlook.com.br>
2023-11-04 14:17:44 -0400

    adicionado repositório
    adicionado repositório vagrant-lab

---
+++ .vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.21.0"]]],"checksum":"2d617e65519fb878a379e04633b3b1dfa7e173a157d4fee0bd4f5cc10340ff28","vagrant_version":"2.4.0"}
